1ec07a1b6b3bbc3e0575b9ea0032d6076b97f070
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2025-01-18 08:59:36 +0530

    Create PTOD18.py

---
+++ PTOD18.py
@@ -0,0 +1,17 @@
+#This code is written in python
+
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+class Solution:
+    def reverseList(self, head):
+        prev = None
+        current = head
+        while current is not None:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        return prev
