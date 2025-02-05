e7da824cf1137fc9b817191fafa67ed564775368
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2025-02-05 09:28:47 +0530

    Create PTOD36.py

---
+++ PTOD36.py
@@ -0,0 +1,9 @@
+#This code is written in python
+
+class Solution:
+    def mirror(self, root):
+        if root is None:
+            return
+        root.left, root.right = root.right, root.left
+        self.mirror(root.left)
+        self.mirror(root.right)
