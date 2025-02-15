d418bbe57ebf958c71a4045f73ff0ff054a36bfe
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2025-02-15 21:55:21 +0530

    Create PTOD46.java

---
+++ PTOD46.java
@@ -0,0 +1,21 @@
+class Solution {
+    Node LCA(Node root, Node n1, Node n2) {
+        // your code here.
+        if(root==null){
+            return null;
+        }
+        
+        if(root.data==n1.data||root.data==n1.data){
+            return root;
+        }
+        
+       if(root.data>n1.data&&root.data>n2.data){
+           return LCA(root.left,n1,n2);
+       }
+       else if(root.data<n1.data&&root.data<n2.data){
+           return LCA(root.right,n1,n2);
+       }
+       
+       return root;
+    }
+}
