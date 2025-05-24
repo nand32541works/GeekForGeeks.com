4f4fe95b8330d55a6007505c34806ec631ae6509
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2025-05-24 09:46:42 +0530

    Create PTOD128.java

---
+++ PTOD128.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int sumSubstrings(String s) {
+        int ans = 0 , n = s.length();
+        for(int i = 0 ; i < n ; i++){
+            ans += helper(s,i,"");
+        }
+        return ans;
+    }
+    public static int helper(String s, int idx , String subString){
+        if(idx == s.length()) {
+            return 0;
+        }
+        subString = new StringBuilder(subString).append(s.charAt(idx)).toString();
+        return Integer.parseInt(subString)+helper(s,idx+1,subString);
+    }
+}
