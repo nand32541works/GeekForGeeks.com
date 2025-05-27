2f16332385e475830dae4bba318807f442dd2743
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2025-05-27 08:01:02 +0530

    Create PTOD132.py

---
+++ PTOD132.py
@@ -0,0 +1,13 @@
+class Solution:
+	def leafNodes(self, preorder):
+		ans = []
+		def collect(arr):
+		    nonlocal ans
+		    if not arr:
+		        return
+		    if len(arr) == 1:
+		        ans.append(arr[0])
+	        collect([x for x in arr if x < arr[0]])
+	        collect([x for x in arr if x > arr[0]])
+        collect(preorder)
+        return ans
