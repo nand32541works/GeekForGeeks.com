7e1b2bbd67f28521b966466d1d85554409e3264b
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2025-05-30 07:09:44 +0530

    Create PTOD134.py

---
+++ PTOD134.py
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMaxFork(Node root, int k) {
+        int ans[]=new int[1];
+        rec(root,k,new int[]{Integer.MAX_VALUE},ans);
+        return ans[0]==0?-1:ans[0];
+    }
+    private void rec(Node root,int k,int[] dis,int[] nei){
+        if(root==null) return;
+        if(k>=root.data && k-root.data<dis[0]){
+            dis[0]=k-root.data;
+            nei[0]=root.data;
+        }
+        rec(root.left,k,dis,nei);
+        rec(root.right,k,dis,nei);
+    }
+}
