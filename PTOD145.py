d9504446da98b8f58626e054fad1895693b8b8b1
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2025-06-12 08:38:25 +0530

    Create PTOD145.py

---
+++ PTOD145.py
@@ -0,0 +1,5 @@
+class Solution:
+    def printKClosest(self, arr, k, x):
+        from heapq import nsmallest
+        k_smallest = nsmallest(k, ((abs(x - a), -a) for a in arr if a != x))
+        return [-a for _, a in k_smallest]
