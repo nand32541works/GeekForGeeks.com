5d7b65db6367781320594f4903f1b425dfa84b5a
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2025-07-26 10:18:23 +0530

    Create PTOD190.java

---
+++ PTOD190.java
@@ -0,0 +1,14 @@
+class Solution {
+    public ArrayList<Integer> findMajority(int[] arr) {
+        int tot = arr.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        TreeMap<Integer,Integer> mp = new TreeMap<>();
+        for(int n:arr){
+            mp.put(n,mp.getOrDefault(n,0)+1);
+        }
+        for(int k:mp.keySet()){
+            if(tot/3 < mp.get(k)) ans.add(k);
+        }
+        return ans;
+    }
+}
