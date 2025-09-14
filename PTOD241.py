d13c421fbb90b75d3b94b7892874c600d039b98e
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2025-09-15 00:07:11 +0530

    Create PTOD241.py

---
+++ PTOD241.py
@@ -0,0 +1,12 @@
+class Solution:
+    def stringStack(self, pat, tar):
+        n, m = len(pat), len(tar)
+        i, j = n - 1, m - 1
+        
+        while i >= 0 and j >= 0:
+            if pat[i] == tar[j]:
+                i -= 1
+                j -= 1
+            else:
+                i -= 2  
+        return j < 0
