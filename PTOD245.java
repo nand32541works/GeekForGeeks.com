e3a533488d807a204b7be34f4bf00c4634ce7728
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2025-09-19 00:14:34 +0530

    Create PTOD245.java

---
+++ PTOD245.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minParentheses(String s) {
+        int open = 0;        
+        int insertions = 0;  
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                open++;
+            } 
+            else { // c == ')'
+                if (open > 0) {
+                    open--; 
+                } 
+                else {
+                    insertions++; 
+                }
+            }
+        }
+        return insertions + open;
+    }
+}
