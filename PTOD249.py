a2b00e47e463fec182a1919acd88c151cb2071f8
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2025-09-23 00:03:19 +0530

    Create PTOD249.py

---
+++ PTOD249.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseQueue(self, q):
+        st = []
+        while q:
+            st.append(q[0])
+            q.popleft()
+        while st:
+            q.append(st.pop())
