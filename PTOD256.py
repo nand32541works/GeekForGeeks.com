2429b71774323a8995b18e93c2649e2b338a7d8a
Nanda Kishore Jana <nandakishorejana780@gmail.com>
2025-10-02 00:07:32 +0530

    Create PTOD256.py

---
+++ PTOD256.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combinationSum(self, n, k):
+        res = []
+        def backtrack(start, path, total):
+            if len(path) == k:
+                if total == n:
+                    res.append(path[:])
+                return
+            for i in range(start, 10):  
+                if total + i > n:
+                    break
+                path.append(i)
+                backtrack(i + 1, path, total + i)  
+                path.pop()
+        backtrack(1, [], 0)
+        return res
